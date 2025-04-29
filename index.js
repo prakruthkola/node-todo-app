4f3824b51a15664c09c73c3dabf3dac4b0ebf737
kola prakruth <prakruthkola98@gmail.com>
2025-04-29 17:45:35 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let todos = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { todos });
+});
+
+app.post('/add', (req, res) => {
+    const todo = req.body.todo;
+    if (todo) {
+        todos.push(todo);
+    }
+    res.redirect('/');
+});
+
+app.post('/delete', (req, res) => {
+    const index = req.body.index;
+    todos.splice(index, 1);
+    res.redirect('/');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
+
